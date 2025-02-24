d6d1db771415efc82702d3b707c2f0f0f495e800
Funmito <adejuwonvictor2004@gmail.com>
2025-02-24 17:28:45 +0100

    Tiny changes

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -1,31 +1,20 @@
+import path from "path";
 import express from "express";
 import serverless from "serverless-http";
 
 var app = express();
 var expressWs = require("express-ws")(app);
 
-app.use(function (req, res, next) {
-  console.log("middleware");
-  req.testing = "testing";
-  return next();
-});
-
 app.use(express.static("public"));
 
 app.get("/", function (req, res, next) {
-  console.log("get route", req.testing);
-  res.sendFile("./public/index.html");
-  res.end();
+  res.sendFile(path.join(__dirname, "../.././public/index.html"));
 });
 
 app.ws("/", function (ws, req) {
-  ws.on("open", function () {
-    console.log("opened");
-  });
   ws.on("message", function (message) {
     ws.send(message);
   });
-  console.log("socket", req.testing);
 });
 
 app.listen(3000);
